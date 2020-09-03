5e260aef84663d7b73d8ecd23b64e778597625ca
devrootsubdomain <60580163+devrootsubdomain@users.noreply.github.com>
2020-09-03 12:35:37 +0530

    Update myfile.sh

--- myfile.sh
+++ myfile.sh
@@ -88,3 +88,43 @@ echo "${BLUE} Done Install Golang ${RESET}"
 echo ""
 echo ""
 sar 1 1 >/dev/null
+#sublist3r 
+git clone https://github.com/aboul3la/Sublist3r.git Sublist3r
+cd Sublist3r
+sudo pip3 install -r requirements.txt
+cd ~
+#subfinder 
+go get -u github.com/projectdiscovery/subfinder/cmd/subfinder
+#assetfinder
+go get -u github.com/tomnomnom/assetfinder
+#gospider       
+go get -u github.com/jaeles-project/gospider
+#findlinux 
+wget https://github.com/Edu4rdSHL/findomain/releases/download/2.1.1/findomain-linux
+chmod +x findomain-linux
+cp findomain-linux /usr/bin/findomain
+#gitsub 
+wget https://raw.githubusercontent.com/gwen001/github-search/master/github-subdomains.py
+chmod +x github-subdomains.py
+cp github-subdomains.py /usr/bin/gitsub
+#nuclei 
+go get -u github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+#httpx 
+go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
+#anew 
+go get -u github.com/tomnomnom/anew
+#waybaskurls 
+go get github.com/tomnomnom/waybackurls
+#gobuster
+go get -u github.com/OJ/gobuster
+#wfuzz
+apt-fast install wfuzz
+#aquatone
+go get -u github.com/michenriksen/aquatone 
+#massdsns
+git clone https://github.com/blechschmidt/massdns.git ~/massdns
+cd ~/massdns
+make 
+cd 
+#Subover
+go get -u github.com/Ice3man543/SubOver
